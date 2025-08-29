9cf7ea8a71e61c846cdc48fa959067f2586b6e6e
Roflaff <kimsp0317@gmail.com>
2025-08-29 17:28:44 +0900

    [init]

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 
 from Sentinel.utils.logger import error_logger
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
 if __name__ == "__main__":
     try:
         # Your main application logic here
